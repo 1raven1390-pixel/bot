5ca655b7b495b9cb5589e78efc0d6e19efdeb1a7
1raven1390-pixel <1raven1390@gmail.com>
2026-05-04 14:12:14 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ from threading import Thread
 TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = 8521801987
 CHANNEL_ID = "@rafe_filter_A"
+CHANNEL_ID = "@lostmooz"
 GROUP_ID = "@GP_config_A" # اضافه شد
 SUPPORT_ID = "@Amir_confing_meli"
 
